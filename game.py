b20f7c9657c901448b3667e953e875eafcf29542
JacobsGitRepos <148696634+JacobsGitRepos@users.noreply.github.com>
2023-10-22 16:38:34 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,45 +1,31 @@
 from microbit import *
 import random
 import time
-score = 0
 
-while True:
-    display.show(Image('00900:'
-                       '00900:'
-                       '00900:'
-                       '00900:'
-                       '00900'))
-    side = random.randint(1,2)
+score = 0
+game_over = False
+
+while not game_over:
+    display.show(Image('00900:' * 5))
     time.sleep(1)
+
+    side = random.randint(1, 2)
+
     if side == 1:
-        display.show(Image('00900:'
-                           '00900:'
-                           '90900:'
-                           '00900:'
-                           '00900'))
+        display.show(Image('00900:00900:90900:00900:00900'))
         time.sleep(1)
         if button_a.was_pressed():
-           display.show(Image('00900:'
-                              '00900:'
-                              '00900:'
-                              '00900:'
-                              '00900'))
-           score += 1
+            score += 1
         else:
-            display.show(score)
+            game_over = True  # End the game on incorrect or missed input
     else:
-        display.show(Image('00900:'
-                           '00900:'
-                           '00909:'
-                           '00900:'
-                           '00900'))
+        display.show(Image('00900:00900:00909:00900:00900'))
         time.sleep(1)
         if button_b.was_pressed():
-           display.show(Image('00900:'
-                              '00900:'
-                              '00900:'
-                              '00900:'
-                              '00900'))
-           score += 1
+            score += 1
         else:
-            display.scroll(score)
+            game_over = True  # End the game on incorrect or missed input
+        display.clear()
+
+# Show the final score at the end of the game
+display.show(score)
